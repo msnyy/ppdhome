aa3d77934b1e0028438eb77e378852b2e603ad7b
Yanisa Phanmaisri <muei@MacBook-Pro-khxng-Yanisa.local>
2026-04-11 22:04:45 +0700

    edit images

--- src/app/ppdhome/user/aboutAll/aboutOrganiz/page.js
+++ src/app/ppdhome/user/aboutAll/aboutOrganiz/page.js
@@ -9,7 +9,7 @@ export default function AboutOrganiz() {
             <div className="flex flex-col items-center bg-white lg:mx-20 md:mx-9 mx-4 lg:mt-10 md:mt-6 mt-4 rounded-xl lg:py-10 md:py-6 py-4 shadow-lg mb-10 text-black">
                 <MotionWrapper>
                 <p className="lg:text-4xl md:text-3xl text-xl text-shadow-lg text-center">โครงสร้างหน่วยงาน</p>
-                <Image src={getSupabaseImage("pic/organiz.jpg")} alt="โครงสร้างองค์กร" width={1200} height={700} className="lg:mt-10 md:mt-6 mt-4"></Image>
+                <Image src={getSupabaseImage("pic/Organniz.jpg")} alt="โครงสร้างองค์กร" width={1200} height={700} className="lg:mt-10 md:mt-6 mt-4"></Image>
                 </MotionWrapper>
             </div>
         </section>
